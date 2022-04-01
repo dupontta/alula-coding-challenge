1ed3ee534a1970477fea2714d987df9a6dc946d1
Trevor Dupont <dupont.trevor.a@gmail.com>
2022-04-01 09:31:18 -0500

    add middleware

--- index.js
+++ index.js
@@ -8,46 +8,60 @@ const apiKey = '8GnehPnpWwqfbc160QASxGU5JTF7wk7ZySZagQiU';
 
 app.use(express.json());
 
-app.get('/asteroids', (req, res) => {
-    let apiUrl = `https://api.nasa.gov/neo/rest/v1/feed?detailed=false&api_key=${apiKey}`;
-    let asteroids = { asteroids: [] };
-    const inputData = req.body;
+const validateInput = (req, res, next) => {
     let startDateCheck = null;
-    let endDateCheck = new Date();
-    let withinValue = 0;
-
+    let endDateCheck = null;
+    let isValid = true;
     // check to see if dateStart is present in request body, and check that it is a number
-    if (inputData.hasOwnProperty('dateStart')) {
-        startDateCheck = new Date(inputData.dateStart);
+    if (req.body.hasOwnProperty('dateStart')) {
+        startDateCheck = new Date(req.body.dateStart);
         if (isNaN(startDateCheck.getTime())) {
-            throw new Error('Start date is not a date.');
+            isValid = false;
+            res.status(400).send('Start date is not a date. Please change to be a date.');
+            // throw new Error('Start date is not a date. Please change to be a date.');
         }
     } else {
-        throw new Error('Start date not present.');
+        isValid = false;
+        res.status(400).send('Start date not present. Please add start date.');
     }
 
     // check that dateEnd it is a number if included since endDate is 'optional' and defaults to today
-    if (inputData.hasOwnProperty('dateEnd')) {
-        endDateCheck = new Date(inputData.dateEnd);
+    if (req.body.hasOwnProperty('dateEnd')) {
+        endDateCheck = new Date(req.body.dateEnd);
         if (isNaN(endDateCheck.getTime())) {
-            throw new Error('End date is not a date.');
+            isValid = false;
+            res.status(400).send('End date is not a date. Please change to be a date.');
         }
     }
 
     // check that the within value is there so we can filter
-    if (inputData?.within?.value) {
-        if (!isNaN(inputData.within.value)) {
-            if (inputData.within.value > 0) {
-                withinValue = inputData.within.value;
-            } else {
-                throw new Error('Within value is negative, must be greater than 0.');
+    if (req.body?.within?.value) {
+        if (!isNaN(req.body.within.value)) {
+            if (req.body.within.value < 0) {
+                isValid = false;
+                res.status(400).send('Within value is negative, must be greater than 0.');
             }
         } else {
-            throw new Error('Within value is not a number.');
+            isValid = false;
+            res.status(400).send('Within value is not a number.');
         }
     } else {
-        throw new Error('Within value(s) missing.');
+        isValid = false;
+        res.status(400).send('Within value(s) missing. Please add within values.');
     }
+
+    if (isValid) {
+        next();
+    }
+};
+
+app.get('/asteroids', validateInput, (req, res, next) => {
+    let apiUrl = `https://api.nasa.gov/neo/rest/v1/feed?detailed=false&api_key=${apiKey}`;
+    let asteroids = { asteroids: [] };
+    const inputData = req.body;
+    let startDateCheck = new Date(inputData.dateStart);
+    let endDateCheck = inputData.dateEnd ? new Date(inputData.dateEnd) : new Date();
+    let withinValue = inputData.within.value;
 
     // check the number of days in the date span. If more than 7 days, split up and make multiple calls.
     if (getNumberOfDays(startDateCheck, endDateCheck) > 7) {
@@ -65,10 +79,10 @@ app.get('/asteroids', (req, res) => {
                 });
 
                 asteroids.asteroids = asteroids;
-                res.send(asteroids);
+                res.status(200).json(asteroids);
             })
             .catch((err) => {
-                throw new Error(err.response.data.message);
+                next(err);
             });
     } else {
         console.log('---Getting Data---');
@@ -77,14 +91,22 @@ app.get('/asteroids', (req, res) => {
         getAsteroidData(apiUrl, withinValue)
             .then((data) => {
                 asteroids.asteroids = data;
-                res.send(asteroids);
+                res.status(200).json(asteroids);
             })
             .catch((err) => {
-                throw new Error(err.response.data.message);
+                next(err);
             });
     }
 });
 
+app.use('/asteroids', (err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send(err);
+});
+
+app.listen(serverPort, () => console.log(`Started Server at http://localhost:${serverPort}!`));
+
+// -------------------------------------------------------------------------------
 const getNumberOfDays = (startDate, endDate) => {
     let timeDifference = endDate.getTime() - startDate.getTime();
     return timeDifference / (1000 * 3600 * 24);
@@ -133,5 +155,3 @@ const filterAndMapAsteroids = (res, asteroidsArray, withinValue) => {
         asteroidsArray.push(...filteredArray);
     });
 };
-
-app.listen(serverPort, () => console.log(`Started Server at http://localhost:${serverPort}!`));
