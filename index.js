941bfdbb81691511560fb56b4c25c63da24bf7b7
Trevor Dupont <dupont.trevor.a@gmail.com>
2022-04-01 11:24:20 -0500

    remove path from error middleware

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get('/asteroids', validateInput, (req, res, next) => {
     }
 });
 
-app.use('/asteroids', (err, req, res, next) => {
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.send(err);
 });
